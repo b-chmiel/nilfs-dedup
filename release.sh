08c8e943f2ea09399922978362b153bf6a9854be
Bartłomiej Chmiel <bachm44@gmail.com>
2023-05-26 16:11:12 +0200

    fix: release script artifact naming convention

--- release.sh
+++ release.sh
@@ -2,29 +2,33 @@
 
 set -euo pipefail
 
-rm -rf build
-mkdir -pv build
+BUILD_DIR=build
+LINUX_DIR=$BUILD_DIR/linux
+
+rm -rf $BUILD_DIR
+mkdir -pv $BUILD_DIR
 
 git clone \
 	--depth 1 \
 	--branch nilfs2-dat-indirection-deduplication \
 	file://$(pwd)/workflow/linux \
-	build/linux
+	$LINUX_DIR
 
 RELEASE_NAME=nilfsdedup-$(git rev-list --max-count=1 --abbrev-commit HEAD)
-KRELEASE_NAME=$(make -C build/linux LLVM=1 kernelversion)
+KVERSION=$(make -C $LINUX_DIR LLVM=1 kernelversion)
 echo $RELEASE_NAME
 echo $KVERSION
 
-cp config-kernel-debian build/linux/.config
+cp config-kernel-debian $LINUX_DIR/.config
+make -C $LINUX_DIR -j8 modules_prepare
+
 make \
-	-C build/linux \
+	-C $LINUX_DIR \
 	deb-pkg \
 	-j8 \
 	LLVM=1 \
 	DEB_BUILD_OPTIONS=nocheck \
-	LOCALVERSION=-$KVERSION \
-	KDEB_PKGVERSION=$KVERSION-l \
-	V=1
+	LOCALVERSION=-$(cd $LINUX_DIR && git rev-list --max-count=1 --abbrev-commit HEAD) \
+	KDEB_PKGVERSION=$KVERSION-l
 
-gh release create --generate-notes $RELEASE_NAME build/linux-image* 
+gh release create --generate-notes $RELEASE_NAME $BUILD_DIR/linux-image* $BUILD_DIR/linux-headers* $BUILD_DIR/linux-libc-dev*
