fea46d7965dea8c4747bf809b983609626e19eda
Bartłomiej Chmiel <bachm44@gmail.com>
2023-06-02 23:18:02 +0200

    feat(vagrant): fs_utils script

--- run.sh
+++ run.sh
@@ -9,9 +9,7 @@ vagrant ssh -- -t <<HEREDOC
 set -euo pipefail
 IFS=$'\n\t'
 
-sudo bash /vagrant/setup.sh
 sudo bash /tests/test.sh NILFS2-DEDUP
-sudo bash /vagrant/teardown.sh
 sudo bash /tests/test_nilfs_dedup.sh
 HEREDOC
 
