3ebcf8658f0ab133e89e8c84068c26a9e1de7a00
Bartłomiej Chmiel <bachm44@gmail.com>
2023-06-16 21:42:37 +0200

    feat(vagrant): gc cleanup in fs_utils

--- fs_utils.sh
+++ fs_utils.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -euo pipefail
+set -x
 IFS=$'\n\t'
 
 source /tests/test_env.sh
@@ -43,8 +44,22 @@ destroy_fs() {
 	umount_fs || true
 	rm -fv $FILESYSTEM_FILE
 	sync
-	sleep 5
+	sleep 2
 	umount_fs || true
 	rm -fv $FILESYSTEM_FILE
 	sync
+}
+
+run_gc_cleanup() {
+	echo "Running gc cleanup"
+	echo "Launching cleanerd daemon"
+	nilfs_cleanerd
+	sleep 2
+	echo "Cleaning"
+	nilfs-clean -p 0 -m 0 --verbose --speed 32
+	echo 'Waiting for garbage collection end'
+	timeout 240s bash -c "( tail -n0 -f /var/log/daemon.log &) | grep -q -e 'manual run completed' -e 'wait 10.000000000'"
+	echo 'Garbage collection ended'
+	nilfs-clean --stop
+	nilfs-clean --quit
 }
